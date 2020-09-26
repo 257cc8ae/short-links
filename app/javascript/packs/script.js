f17b887aa59340cc7a4b5b0b5b84c74a97b0f341
Lusaca <kny70dr@gmail.com>
2020-09-26 21:07:59 +0900

    基本機能の完成

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,15 +1,19 @@
-document.getElementById("get-short-url").addEventListener("click",function(){
-    fetch(`/?url=${document.getElementById("short-url").value}`, {
-        method: "get"
-    }).then(function (response) {
-        if (response.status === 200) {
-            response.json().then(data => {
-                console.log(data.url);
-            });
-        } else {
-            console.log(response.statusText);
-        };
-    }).catch(function (response) {
-        console.log(response);
+document.addEventListener("turbolinks:load", function () {
+    const get_link_btn = document.getElementById("get-short-url");
+    const link_field = document.getElementById("short-url");
+    get_link_btn.addEventListener("click", function () {
+        fetch(`/?url=${link_field.value}`, {
+            method: "get"
+        }).then(function (response) {
+            if (response.status === 200) {
+                response.json().then(data => {
+                    link_field.value = data.url;
+                });
+            } else {
+                console.log(response.statusText);
+            };
+        }).catch(function (response) {
+            console.log(response);
+        });
     });
 });
